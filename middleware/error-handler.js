d85ef4afed7dd9a6c28f0b9e660a3b89ccd247ef
Aleksandr <alexbachurin91@gmail.com>
2023-10-09 16:53:47 +0300

    custom error + Duplicate email error handling functionality added

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,22 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  // custom error
+  let customError = {
+    //set defaults
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, try again later",
+  };
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+  // duplicate email error
+  if (err.code && err.code === 11000) {
+    customError.msg = `${Object.keys(err.keyValue)} already in use`;
+    customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
