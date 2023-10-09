e6e1fa1fc3db6566faf6915ac0199f7bda5056fe
Aleksandr <alexbachurin91@gmail.com>
2023-10-09 20:56:04 +0300

    Added swagger UI & docs to project

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 // routers export
@@ -34,6 +39,13 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+//basic route
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+//add swagger ui
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
